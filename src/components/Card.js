225b2aef2b652078ddb4807b55dd0c7a3b285310
Peter Rackley <peter@shared-techs.com>
2022-03-18 16:12:41 -0400

    Updated Chuck's image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import chucknorrisT from './chucknorrisT.png';
 
 const Card = ({joke, cnum, tot}) => {
 	return (
 		<div>
 			<article className="br2 ba dark-gray b--black-30 mv4 w-100 w-50-m w-25-l mw5 center">
-			  <img src={`https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png`} className='db w-100 br2 br--top' alt='Image of Chuck.' />
+			  <img src={chucknorrisT} className='db center w-75 br2 br--top' alt="Chuck Norris Cartoon figure" />
 			  <div className="pa2 ph3-ns pb3-ns">
 			    <div className="dt w-100 mt1">
-			      <div className="dtc">
-			        <h1 className="f5 f4-ns mv0"></h1>
-			      </div>
 			      <div className="f6 lh-copy ">
 			        <h2 className="center measure mt2 mid-gray f5 mv0">Joke: { cnum + 1} of {tot + 1}</h2>
 			      </div>
@@ -24,17 +22,3 @@ const Card = ({joke, cnum, tot}) => {
 }
 
 export default Card;
-
-
-{/*
-		<div>
-			<img alt='chuck norris joke' src={`https://assets.chucknorris.host/img/avatar/chuck-norris.png`} />
-			<article className="center mw5 mw6-ns hidden ba mv4">
-				<h1 className="f4 bg-near-black white mv0 pv2 ph3">Joke { cnum + 1} of {tot + 1}</h1>
-				<div className="pa3 bt">
-					<p className="f6 f5-ns lh-copy measure mv0">
-						{joke}
-					</p>
-				</div>
-			</article>
-		</div>*/}
